f568993556348203fc9683d4078ff01a830ca1e0
Matt Moore <mtmoore55@gmail.com>
2024-07-29 13:59:44 -0700

    Initial Commit

---
+++ Later/LaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaterApp.swift
+//  Later
+//
+//  Created by Matt Moore on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct LaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
